c7955e6f2500302d24fe44a7ded1852ad15b1a6e
Bryan-Nelson-2 <bryan.nelson@stu.ocsb.ca>
2025-10-26 19:17:48 -0400

    This program determines the light level and lights up the corresponding neopixels

--- main.ts
+++ main.ts
@@ -2,7 +2,34 @@
  *
  * Created by: Alex
  * Created on: Oct 2025
- * This program ...
+ * This program determines the light level and lights up the corresponding neopixels
 */
 
-basic.showString('Hello, World!')
+// variables
+let lightLevel = input.lightLevel()
+let neopixelStrip: neopixel.Strip = null
+
+// determines the light level and lights up the corresponding neopixels
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+if (lightLevel > 208) {
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+}
+else if (lightLevel > 156) {
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+}
+else if (lightLevel > 104) {
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+}
+else if (lightLevel > 52) {
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+}
+else if (lightLevel <= 51) {
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+}
+neopixelStrip.show()
